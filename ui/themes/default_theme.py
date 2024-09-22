ee81916e99097250f067d4dc1f5e0555bfb9c970
AYUHIK <chaki20091@gmail.com>
2024-09-22 19:42:21 +0300

    themes for interface

---
+++ ui/themes/default_theme.py
@@ -0,0 +1,40 @@
+def def_theme(self):
+    self.setStyleSheet("background-color: #1C1C1C;")
+    self.title_bar.setStyleSheet("background-color: #2E2E2E;")
+    self.winlocker_name.setStyleSheet("font-size: 40px; background-color: #2E2E2E;")
+    self.main_frame.setStyleSheet("font-size: 40px; background-color: #212121; border-radius: 15px;")
+    self.second_frame.setStyleSheet("font-size: 40px; background-color: #212121; border-radius: 15px;")
+    self.third_frame.setStyleSheet("font-size: 40px; background-color: #212121; border-radius: 15px;")
+
+    self.main_title_frame.setStyleSheet("background-color: #2E2E2E; border-radius: 8px;")
+    self.main_title_text.setStyleSheet("font-size: 18px; background-color: #2E2E2E;")
+    
+    self.main_text_frame.setStyleSheet("background-color: #2E2E2E; border-radius: 8px;")
+    self.main_text.setStyleSheet("font-size: 18px; background-color: #2E2E2E;")
+    
+    self.code_frame.setStyleSheet("background-color: #2E2E2E; border-radius: 8px;")
+    self.code_text.setStyleSheet("font-size: 15px; background-color: #2E2E2E;")
+    
+    self.tries_frame.setStyleSheet("background-color: #2E2E2E; border-radius: 8px;")
+    self.tries_text.setStyleSheet("font-size: 15px; background-color: #2E2E2E;")
+    
+    self.countdown_frame.setStyleSheet("background-color: #2E2E2E; border-radius: 8px;")
+    self.countdown_text.setStyleSheet("font-size: 15px; background-color: #2E2E2E;")
+    self.hour_timer.setStyleSheet("font-size: 15px; background-color: #2E2E2E;")
+    self.minutes_timer.setStyleSheet("font-size: 15px; background-color: #2E2E2E;")
+    
+    self.change_theme.setStyleSheet("QPushButton {font-size: 20px; background-color: #2E2E2E; border-radius: 8px;} QPushButton:hover {background-color: #2A2A2A;}")
+    self.about_button.setStyleSheet("QPushButton {font-size: 25px; background-color: #2E2E2E; border-radius: 8px;} QPushButton:hover {background-color: #2A2A2A;}")
+    
+    self.about_frame.setStyleSheet("background-color: #1C1C1C;")
+    self.main_about_frame.setStyleSheet("font-size: 40px; background-color: #212121; border-radius: 15px;")
+    self.close_button.setStyleSheet("QPushButton {font-size: 25px; background-color: #2E2E2E; border-radius: 8px;} QPushButton:hover {background-color: #2A2A2A;}")
+    self.first_text.setStyleSheet("font-size: 23px; background-color: #212121;")
+    self.second_text.setStyleSheet("font-size: 22px; background-color: #212121;")
+    self.second_about_frame.setStyleSheet("font-size: 40px; background-color: #212121; border-radius: 15px;")
+    self.telegram_link.setStyleSheet("font-size: 30px; background-color: #212121;")
+    self.discord_link.setStyleSheet("font-size: 30px; background-color: #212121;")
+    self.github_link.setStyleSheet("font-size: 30px; background-color: #212121;")
+    
+    self.reset_button.setStyleSheet("QPushButton {font-size: 33px; background-color: #2E2E2E; border-radius: 8px;} QPushButton:hover {background-color: #2A2A2A;}")
+    self.build_button.setStyleSheet("QPushButton {font-size: 33px; background-color: #2E2E2E; border-radius: 8px;} QPushButton:hover {background-color: #2A2A2A;}")
---
+++ ui/themes/northern_lights_theme.py
@@ -0,0 +1,40 @@
+def northern_theme(self):
+    self.setStyleSheet("background-color: #021C1E;")
+    self.title_bar.setStyleSheet("background-color: #2C7873;")
+    self.winlocker_name.setStyleSheet("font-size: 40px; background-color: #2C7873;")
+    self.main_frame.setStyleSheet("font-size: 40px; background-color: #004445; border-radius: 15px;")
+    self.second_frame.setStyleSheet("font-size: 40px; background-color: #004445; border-radius: 15px;")
+    self.third_frame.setStyleSheet("font-size: 40px; background-color: #004445; border-radius: 15px;")
+    
+    self.main_title_frame.setStyleSheet("background-color: #2C7873; border-radius: 8px;")
+    self.main_title_text.setStyleSheet("font-size: 18px; background-color: #2C7873;")
+    
+    self.main_text_frame.setStyleSheet("background-color: #2C7873; border-radius: 8px;")
+    self.main_text.setStyleSheet("font-size: 18px; background-color: #2C7873;")
+    
+    self.code_frame.setStyleSheet("background-color: #2C7873; border-radius: 8px;")
+    self.code_text.setStyleSheet("font-size: 15px; background-color: #2C7873;")
+    
+    self.tries_frame.setStyleSheet("background-color: #2C7873; border-radius: 8px;")
+    self.tries_text.setStyleSheet("font-size: 15px; background-color: #2C7873;")
+    
+    self.countdown_frame.setStyleSheet("background-color: #2C7873; border-radius: 8px;")
+    self.countdown_text.setStyleSheet("font-size: 15px; background-color: #2C7873;")
+    self.hour_timer.setStyleSheet("font-size: 15px; background-color: #2C7873;")
+    self.minutes_timer.setStyleSheet("font-size: 15px; background-color: #2C7873;")
+    
+    self.change_theme.setStyleSheet("QPushButton {font-size: 20px; background-color: #2C7873; border-radius: 8px;} QPushButton:hover {background-color: #276A66;}")
+    self.about_button.setStyleSheet("QPushButton {font-size: 25px; background-color: #2C7873; border-radius: 8px;} QPushButton:hover {background-color: #276A66;}")
+    
+    self.about_frame.setStyleSheet("background-color: #021C1E;")
+    self.main_about_frame.setStyleSheet("font-size: 40px; background-color: #004445; border-radius: 15px;")
+    self.close_button.setStyleSheet("QPushButton {font-size: 25px; background-color: #2C7873; border-radius: 8px;} QPushButton:hover {background-color: #276A66;}")
+    self.first_text.setStyleSheet("font-size: 23px; background-color: #004445;")
+    self.second_text.setStyleSheet("font-size: 22px; background-color: #004445;")
+    self.second_about_frame.setStyleSheet("font-size: 40px; background-color: #004445; border-radius: 15px;")
+    self.telegram_link.setStyleSheet("font-size: 30px; background-color: #004445;")
+    self.discord_link.setStyleSheet("font-size: 30px; background-color: #004445;")
+    self.github_link.setStyleSheet("font-size: 30px; background-color: #004445;")
+    
+    self.reset_button.setStyleSheet("QPushButton {font-size: 33px; background-color: #2C7873; border-radius: 8px;} QPushButton:hover {background-color: #276A66;}")
+    self.build_button.setStyleSheet("QPushButton {font-size: 33px; background-color: #2C7873; border-radius: 8px;} QPushButton:hover {background-color: #276A66;}")
